5e5f89ce948fad965e695f87c627e919836ffd48
Richard <richard.kim.0000@gmail.com>
2021-08-27 16:08:32 -0700

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetups";
+
 function App() {
+  // localhost:3000/
+  // my-page.com/
+
   return (
     <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
