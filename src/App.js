a2a49934241ca4ba9d20b934a710fef6c0cad9d2
Richard <richard.kim.0000@gmail.com>
2021-08-29 16:35:12 -0700

    Started nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetups";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   // localhost:3000/
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
