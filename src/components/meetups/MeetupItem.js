89fd2e2f6929058a4e974b3f6e1098bfeaf317ee
Richard <richard.kim.0000@gmail.com>
2021-08-30 14:31:04 -0700

    Improvements to be made

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,28 @@
+import { useContext } from 'react';
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
+import FavoritesContext from '../../store/favorites-context';
 
 const MeetupItem = (props) => {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  const toggleFavoritesStatusHander = () => {
+    if(itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    }
+    else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -14,7 +35,7 @@ const MeetupItem = (props) => {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>Add to Favorites</button>
+          <button onClick={toggleFavoritesStatusHander}>{itemIsFavorite ? 'Remove from Favorites' : 'Add to Favorites'}</button>
         </div>
       </Card>
     </li>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useContext } from "react";
 
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 const Favorites = () => {
-    return (
-        <div>
-            Favorites Page
-        </div>
-    )
-}
+  const favoritesCtx = useContext(FavoritesContext);
 
-export default Favorites
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You have no favorites</p>;
+  } else {
+    <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+  return (
+    <section>
+      <h1>My Favorite Meetups</h1>
+      {content}
+    </section>
+  );
+};
+
+export default Favorites;
