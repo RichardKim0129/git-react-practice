0a2f00fcae1918f61a3c5b440cec3d3fb620321e
Richard <richard.kim.0000@gmail.com>
2021-08-29 21:09:03 -0700

    Learning database

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-    return (
-        <div>
-            New Meetup Page
-        </div>
-    )
-}
+  const history = useHistory();
 
-export default NewMeetupPage
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-getting-started-45f0e-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
+
+export default NewMeetupPage;
